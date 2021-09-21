afd4245eb394be760b5dac6b9ca7b5185b95be0e
arbi1994 <arbi.1994@hotmail.com>
2021-09-21 12:38:20 +1000

    Removed dummy data

--- src/components/GenresSelector.js
+++ src/components/GenresSelector.js
@@ -2,31 +2,6 @@ import React, { useState, useEffect } from 'react';
 import tmdb from '../apis/tmdb';
 import { GET, BASE_URL } from '../api_config';
 
-const genres = [
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-  {genre: 'Action'},
-]
-
-const genreArr = genres.map(genre => genre.genre)
-console.log(genreArr)
-
 const GenresSelector = () => {
   const [open, setOpen] = useState(false)
   const [genres, setGenres] = useState([])
